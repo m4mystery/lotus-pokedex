616d64986ac9d9b179fba540c86890c7b6069328
m4mystery <mwachalanyambu@Mwachalas-MacBook-Pro.local>
2018-10-07 20:38:31 +0100

    Removed static links for AJAX and Bootstrap
    Moved to cloudshare

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!--CSS-->
-  <link rel="stylesheet" href="Bootstrap/css/bootstrap.min.css">
+    <!--bootstrap 3.3.1 CSS-->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <!--Main Page-->
   <link rel="stylesheet" type="text/css" href="css/PokemonSpecificTyping.css">
     <!--Search bar-->
@@ -15,9 +16,10 @@
     <!--Evolution line-->
   <link rel="stylesheet" type="text/css" href="css/evolutionline.css">
   <!--JS-->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-  <script src="Bootstrap/js/bootstrap.min.js"></script>
+  <!--bootstrap and jquery-->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
   <!--<script src="js/loading.js"></script>-->
   <!--highcharts and bootstrap-->
   <script src="https://code.highcharts.com/highcharts.js"></script>
